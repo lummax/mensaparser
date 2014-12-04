efe2e8969f8af6bc2872eb106d34a6c31e9d9390
lummax <luogpg@googlemail.com>
2014-12-04 13:17:21 +0100

    parser: Extraxt legend-markers from description

--- src/mensaparser/parser.clj
+++ src/mensaparser/parser.clj
@@ -18,7 +18,7 @@
 
 (defn extract-content
   [data selection]
-  (string/replace (string/replace (or (first (:content (first (html/select data selection)))) "") #"\n" " ") #"  " " "))
+  (or (first (:content (first (html/select data selection)))) ""))
 
 (defn transform-date
   [string]
@@ -68,26 +68,42 @@
   [entry selection]
   (string/replace (string/replace (extract-content entry selection) #"," ".") #"€" ""))
 
+(defn transform-description
+  [meal-entry]
+  (map (fn
+         [element]
+         (if
+           (string? element)
+           (string/trim (string/replace (string/replace element #"\n" " ") #"  " " "))
+           (string/split (first (:content element)) #", ")))
+       (:content (first (html/select meal-entry [:td.field-name-field-description])))))
+
 (defn meals-mapper
-  [food-map meal-entry]
-  {:description  (extract-content meal-entry [:td.field-name-field-description])
-   :price-students (extract-price meal-entry [:td.field-name-field-price-students])
-   :price-employees (extract-price meal-entry [:td.field-name-field-price-employees])
-   :notes (map #(get food-map %) (re-seq #"food-type-\p{javaLowerCase}+" (:class (:attrs meal-entry))))})
+  [food-map legend-map meal-entry]
+  (let [description-data (transform-description meal-entry)
+        description (string/join " " (take-nth 2 description-data))
+        notes (distinct (flatten (take-nth 2 (rest description-data))))]
+    {:description description
+     :price-students (extract-price meal-entry [:td.field-name-field-price-students])
+     :price-employees (extract-price meal-entry [:td.field-name-field-price-employees])
+     :notes (concat
+              (map #(get food-map %) (re-seq #"food-type-\p{javaLowerCase}+" (:class (:attrs meal-entry))))
+              (map #(get legend-map %) notes))}))
 
 (defn categories-mapper
-  [food-map category-table]
+  [food-map legend-map category-table]
   {:name (extract-content category-table [:th.category-name])
-   :meals (map #(meals-mapper food-map %) (html/select category-table [:tr.node]))})
+   :meals (map #(meals-mapper food-map legend-map %) (html/select category-table [:tr.node]))})
 
 (defn food-plan-mapper
-  [date-map food-map food-plan]
+  [date-map food-map legend-map food-plan]
   {:date (get date-map (:id (:attrs food-plan)))
-   :categories (map #(categories-mapper food-map %) (html/select food-plan [:table.food-category]))})
+   :categories (map #(categories-mapper food-map legend-map %) (html/select food-plan [:table.food-category]))})
 
 (defn generate-food-plan
   [url]
   (let [food-plan-data (get-food-plan-data url)
         date-map (generate-date-map food-plan-data)
-        food-map (generate-food-map food-plan-data)]
-    (map #(food-plan-mapper date-map food-map %) (html/select food-plan-data [:div.food-plan]))))
+        food-map (generate-food-map food-plan-data)
+        legend-map (generate-legend-map food-plan-data)]
+    (map #(food-plan-mapper date-map food-map legend-map %) (html/select food-plan-data [:div.food-plan]))))
